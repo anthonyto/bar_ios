8a62b6b3e5c6785b630800c517f0da9b2cda494d
Anthony To <to.anthony93@gmail.com>
2014-02-28 19:51:39 -0600

    Initial Commit

---
+++ bar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  bar
+//
+//  Created by Anthony To on 2/28/14.
+//  Copyright (c) 2014 Anthony To. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
